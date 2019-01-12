75601cddb517993d5e6ed9da622c3e7b04ff5cd6
Andy Collins <andy.collins@relode.com>
2019-01-12 17:39:30 -0600

    giphy improvements and removing api key from repo

---
+++ HogwartsRunningClub/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HogwartsRunningClub
+{
+    public class Config
+    {
+        public string GiphyApiKey { get; set; }
+    }
+}
